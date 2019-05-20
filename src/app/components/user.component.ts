edde59de44038180fc6df44e58e78101476d685a
Huong <huongnguyenak96@gmail.com>
2019-05-20 19:33:39 +0700

    ngIf else

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user',
-    template: `
-        <h3>User Component</h3>
-        <p [style.color]="textRed">{{name}}</p>
-        <p [class]='myClass'>Name</p>
-        <p>{{age}}</p>
-    `
+    templateUrl: './user.component.html'
+    // template: `
+    //     <h3>User Component</h3>
+
+    //     <p [style.color]='1==2 ? textBlue : textRed'>{{name}}</p>
+    //     <p [class]='myClass'>Name</p>
+    //     <div *ngIf=isDisabled>
+    //         <input disabled placeholder="Enter name 01">
+    //     </div>
+    //     <div>
+    //         <input placeholder="Enter name 02">
+    //     </div>
+    //     <p>{{age}}</p>
+    //     <br><br><br><br><br><br><br>
+    // `
     ,
     styles: [
         `
@@ -24,6 +33,8 @@ export class UserComponent {
     name = 'Huong';
     age: number;
     textRed = 'red';
+    textBlue = 'blue';
+    isDisabled = false;
 
     myClass = 'text-danger text-large';
 
