cfdf64c7da89d48f05bb5bf7bcdaf13e6a22a3e7
Huong <huongnguyenak96@gmail.com>
2019-05-20 18:42:15 +0700

    user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: '.app-root',
+  // templateUrl: './app.component.html',
+  template: `
+    <h2>Hello Angular</h2>
+    <p>{{title}}</p>
+    <p>{{title}}</p>
+  `,
+  // styleUrls: ['./app.component.css']
+  styles: [
+    `
+    p{
+      font-size: 20px;
+      color: red;
+    }
+    `
+  ]
 })
 export class AppComponent {
   title = 'ng1503';
