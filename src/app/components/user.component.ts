8e5a3e37a81c12c0b63a5a576e32a39ad725a054
Huong <huongnguyenak96@gmail.com>
2019-05-22 18:10:24 +0700

    data binding, style binding, prop binging

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -5,17 +5,43 @@ import { Component } from '@angular/core';
     // templateUrl: './user.component.html'
     template: `
         <h3>User Component</h3>
-        <!-- data binding-->
+        <!-- data binding -->
         <p>{{ name }}</p>
+
+        <!-- style binding -->
+        <p [style.color]="1==2 ? redColor : blueColor" >Your name</p>
+
+        <!-- property binding -->
+        <p [id]="myId">Your name</p>
+        <p [class]="myClass">Your name</p>
+        <input [disabled]='isDisabled'  value="KPT" placeholder="Enter your name">
+
 
         <br><br><br><br><br><br><br>
     `
     ,
     styles: [
+        `
+        #your-name {
+            color: green;
+            font-family: Tahoma
+        }
+        .text-danger{
+            color: red
+        }
+        .text-bold{
+            font-weight: bold
+        }
+        `
     ]
 })
 export class UserComponent {
     name = 'Teo';
+    redColor = 'red';
+    blueColor = 'blue';
+    myId = 'your-name';
+    myClass = 'text-danger text-bold';
+    isDisabled = true;
 
     constructor() {
     }
