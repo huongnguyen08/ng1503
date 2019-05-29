77577908c988cd511413662edd769055fabadec8
Huong <huongnguyenak96@gmail.com>
2019-05-29 19:15:03 +0700

    ngModel

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,6 +12,7 @@ export class ProductComponent implements OnInit {
   products: Product[] = listProduct;
   isShowForm = false;
   formAddProduct: FormGroup;
+  filterMode = 'SHOW_ALL';
 
   constructor(private fb: FormBuilder) { }
   ngOnInit() {
