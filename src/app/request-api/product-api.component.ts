9da88c8af524a2e09985e6ce7f1746222f6230c1
Huong <huongnguyenak96@gmail.com>
2019-06-07 18:42:15 +0700

    request local

--- src/app/request-api/product-api.component.ts
+++ src/app/request-api/product-api.component.ts
@@ -7,6 +7,12 @@ import { HttpClient } from '@angular/common/http';
     `
 })
 export class ProductApiComponent {
+    url = 'http://localhost:3000/';
+
     constructor(private http: HttpClient) {
+        this.http.get(this.url)
+        .toPromise()
+        .then(result => console.log(result))
+        .catch(err => console.log(err.message));
     }
 }
