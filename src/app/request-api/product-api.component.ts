c71609dcdf25f255fea22b4d5a0c05b4f988294d
Huong <huongnguyenak96@gmail.com>
2019-06-10 18:30:03 +0700

    INIT_PRODUCT_API

--- src/app/request-api/product-api.component.ts
+++ src/app/request-api/product-api.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { Product } from '../products-list';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
 
 @Component({
     selector: 'app-product-api',
@@ -11,21 +12,30 @@ export class ProductApiComponent {
     listProduct: Product[];
     formAddProduct: FormGroup;
 
-    constructor(private productService: ProductService, private fb: FormBuilder) {
+    constructor(private productService: ProductService, private fb: FormBuilder, private store: Store<any>) {
         this.formAddProduct = this.fb.group({
             name : ['', Validators.required],
             price : ['0', Validators.required]
         });
 
         this.productService.getListProduct()
-        .then(result => this.listProduct = result)
+        .then(result => {
+            this.store.dispatch({
+                type: 'INIT_PRODUCT_API',
+                products: result
+            });
+        })
         .catch(err => console.log({ err: err.message}));
+
+        this.store.select('productsAPI').subscribe(
+            list => this.listProduct = list
+        );
     }
     addProduct() {
         const { name, price } = this.formAddProduct.value;
         this.productService.addProduct(name, price)
         .then(product => {
-            
+
         })
     }
 }
